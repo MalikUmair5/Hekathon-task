369d1b3ae1fbbf7f7001c167e3f86f9f65add926
MalikUmair5 <malikumair.dev@gmail.com>
2024-09-07 14:57:43 -0700

    Completed Milestone 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // ? Milestone 1
+var _a;
 var toogle_button = document.getElementById("toogle_button");
 var general_skills = document.getElementById("general_skills");
 toogle_button === null || toogle_button === void 0 ? void 0 : toogle_button.addEventListener("click", function () {
@@ -13,5 +14,190 @@ toogle_button === null || toogle_button === void 0 ? void 0 : toogle_button.addE
             general_skills.style.display = "block";
             toogle_button.innerText = "Hide General Skills";
         }
+    }
+});
+// ? Milestone 2
+var listingforexperince = [];
+var listingfortechnicalskills = [];
+var listingforgeneralskills = [];
+var listingforlanguages = [];
+var listingforhobbies = [];
+document.body.addEventListener("click", function (e) {
+    var _a, _b, _c, _d, _e, _f;
+    var add_button = e.target;
+    if (add_button.classList.contains("add_button")) {
+        e.preventDefault();
+        console.log(add_button);
+        var previousSibling = (_a = add_button === null || add_button === void 0 ? void 0 : add_button.previousSibling) === null || _a === void 0 ? void 0 : _a.previousSibling;
+        if (previousSibling.value === "") {
+            console.log("Please enter the value");
+            previousSibling.placeholder = "Please enther the value";
+            previousSibling.style.borderBottom = "2px solid red";
+        }
+        else {
+            console.log("hello Pakistan");
+            console.log();
+            if (((_b = previousSibling.previousElementSibling) === null || _b === void 0 ? void 0 : _b.innerHTML) == "Experience:") {
+                listingforexperince.push(previousSibling.value);
+                console.log(listingforexperince);
+                previousSibling.value = "";
+                previousSibling.placeholder = "Enter your experience";
+                previousSibling.style.borderBottom = "2px solid black";
+                var newHtml = listingforexperince
+                    .map(function (experience, id) {
+                    return "<li id=\"experience-".concat(id, "\">").concat(experience, "\n              <button data-id=\"").concat(id, "\" class=\"del_button\">Delete</button>\n            </li>");
+                })
+                    .join("");
+                var divContainer = add_button.nextElementSibling;
+                divContainer.innerHTML = "<ul>".concat(newHtml, "</ul>");
+            }
+            if (((_c = previousSibling.previousElementSibling) === null || _c === void 0 ? void 0 : _c.innerHTML) == "Technical Skills:") {
+                listingfortechnicalskills.push(previousSibling.value);
+                console.log(listingfortechnicalskills);
+                previousSibling.value = "";
+                previousSibling.placeholder = "Enter your the value";
+                previousSibling.style.borderBottom = "2px solid black";
+                var newHtml = listingfortechnicalskills
+                    .map(function (item, id) {
+                    return "<li id=\"experience-".concat(id, "\">").concat(item, "\n              <button data-id=\"").concat(id, "\" class=\"del_button\">Delete</button>\n            </li>");
+                })
+                    .join("");
+                var divContainer = add_button.nextElementSibling;
+                divContainer.innerHTML = "<ul>".concat(newHtml, "</ul>");
+            }
+            if (((_d = previousSibling.previousElementSibling) === null || _d === void 0 ? void 0 : _d.innerHTML) == "General Skills:") {
+                listingforgeneralskills.push(previousSibling.value);
+                console.log(listingforexperince);
+                previousSibling.value = "";
+                previousSibling.placeholder = "Enter your the value";
+                previousSibling.style.borderBottom = "2px solid black";
+                var newHtml = listingforgeneralskills
+                    .map(function (item, id) {
+                    return "<li id=\"experience-".concat(id, "\">").concat(item, "\n              <button data-id=\"").concat(id, "\" class=\"del_button\">Delete</button>\n            </li>");
+                })
+                    .join("");
+                var divContainer = add_button.nextElementSibling;
+                divContainer.innerHTML = "<ul>".concat(newHtml, "</ul>");
+            }
+            if (((_e = previousSibling.previousElementSibling) === null || _e === void 0 ? void 0 : _e.innerHTML) == "Hobbies:") {
+                listingforhobbies.push(previousSibling.value);
+                console.log(listingforhobbies);
+                previousSibling.value = "";
+                previousSibling.placeholder = "Enter your the value";
+                previousSibling.style.borderBottom = "2px solid black";
+                var newHtml = listingforhobbies
+                    .map(function (item, id) {
+                    return "<li id=\"experience-".concat(id, "\">").concat(item, "\n              <button data-id=\"").concat(id, "\" class=\"del_button\">Delete</button>\n            </li>");
+                })
+                    .join("");
+                var divContainer = add_button.nextElementSibling;
+                divContainer.innerHTML = "<ul>".concat(newHtml, "</ul>");
+            }
+            if (((_f = previousSibling.previousElementSibling) === null || _f === void 0 ? void 0 : _f.innerHTML) == "Languages:") {
+                listingforlanguages.push(previousSibling.value);
+                console.log(listingforlanguages);
+                previousSibling.value = "";
+                previousSibling.placeholder = "Enter your the value";
+                previousSibling.style.borderBottom = "2px solid black";
+                var newHtml = listingforlanguages
+                    .map(function (item, id) {
+                    return "<li id=\"experience-".concat(id, "\">").concat(item, "\n              <button data-id=\"").concat(id, "\" class=\"del_button\">Delete</button>\n            </li>");
+                })
+                    .join("");
+                var divContainer = add_button.nextElementSibling;
+                divContainer.innerHTML = "<ul>".concat(newHtml, "</ul>");
+            }
+        }
+    }
+});
+// Personal Information
+var user_name = document.getElementById("name_input");
+var profile_picture_input = document.getElementById("profile_picture_input");
+var contactNumber = document.getElementById("contact_number_input");
+var email = document.getElementById("email_input");
+var dob = document.getElementById("dob_input");
+// Family Information
+var fatherName = document.getElementById("father_name_Input");
+var cnic = document.getElementById("cnic_input");
+// Professional Information
+var careerObjective = document.getElementById("career_objective_input");
+var experience = document.getElementById("experience");
+var technicalSkills = document.getElementById("technical_skills_input");
+var generalSkills = document.getElementById("general_skills_input");
+// Additional Information
+var languages = document.getElementById("languages_input");
+var hobbies = document.getElementById("hobbies_input");
+var religion = document.getElementById("religion_input");
+var maritalStatus = document.getElementById("marital_status_input");
+var name_heading = document.getElementById("name_heading");
+var profile_picture = document.getElementById("profile_picture");
+var user_contact = document.getElementById("user_contact");
+var user_email = document.getElementById("user_email");
+var user_Language = document.getElementById("user_Language");
+var user_hobbies = document.getElementById("user_hobbies");
+var career_objective = document.getElementById("career_objective");
+var father_name = document.getElementById("father_name");
+var user_DOB = document.getElementById("user_DOB");
+var user_cnic = document.getElementById("user_cnic");
+var user_maritalStatus = document.getElementById("user_maritalStatus");
+var user_relagion = document.getElementById("user_relagion");
+var user_experience = document.getElementById("user_experience");
+var technical_Skills = document.getElementById("technical_Skills");
+var user_generalSkills = document.getElementById("user_generalSkills");
+(_a = document.getElementById("submit_button")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function (e) {
+    var _a;
+    e.preventDefault();
+    // First, update the profile picture (if any)
+    var file = (_a = profile_picture_input.files) === null || _a === void 0 ? void 0 : _a[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var _a;
+            profile_picture_input.src = (_a = e.target) === null || _a === void 0 ? void 0 : _a.result;
+            if (profile_picture) {
+                profile_picture.src = profile_picture_input.src;
+            }
+        };
+        reader.readAsDataURL(file);
+    }
+    if (name_heading &&
+        user_contact &&
+        user_email &&
+        user_Language &&
+        user_hobbies &&
+        career_objective &&
+        father_name &&
+        user_DOB &&
+        user_cnic &&
+        user_maritalStatus &&
+        user_relagion &&
+        user_experience &&
+        technical_Skills &&
+        user_generalSkills) {
+        name_heading.innerText = user_name.value;
+        user_contact.innerText = contactNumber.value;
+        user_email.innerText = email.value;
+        career_objective.innerText = careerObjective.value;
+        father_name.innerText = fatherName.value;
+        user_DOB.innerText = dob.value;
+        user_cnic.innerText = cnic.value;
+        user_relagion.innerText = religion.value;
+        user_maritalStatus.innerText = maritalStatus.value;
+        // Update the list of languages
+        user_Language.innerHTML = listingforlanguages
+            .map(function (item) { return "<li>".concat(item, "</li>"); })
+            .join("");
+        user_experience.innerHTML = listingforexperince
+            .map(function (item) { return "<li>".concat(item, "</li>"); })
+            .join("");
+        technical_Skills.innerHTML = listingfortechnicalskills
+            .map(function (item) { return "<li>".concat(item, "</li>"); })
+            .join("");
+        user_generalSkills.innerHTML = listingforgeneralskills
+            .map(function (item) { return "<li>".concat(item, "</li>"); })
+            .join("");
+        user_hobbies.innerHTML = listingforhobbies
+            .map(function (item) { return "<li>".concat(item, "</li>"); })
+            .join("");
     }
 });
--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 // ? Milestone 1
 
-
 let toogle_button = document.getElementById("toogle_button");
 let general_skills = document.getElementById("general_skills");
 
@@ -15,5 +14,238 @@ toogle_button?.addEventListener("click", () => {
       general_skills.style.display = "block";
       toogle_button.innerText = "Hide General Skills";
     }
+  }
+});
+
+// ? Milestone 2
+
+let listingforexperince: string[] = [];
+let listingfortechnicalskills: string[] = [];
+let listingforgeneralskills: string[] = [];
+let listingforlanguages: string[] = [];
+let listingforhobbies: string[] = [];
+
+document.body.addEventListener("click", (e) => {
+  let add_button = e.target as HTMLElement;
+  if (add_button.classList.contains("add_button")) {
+    e.preventDefault();
+    console.log(add_button);
+    let previousSibling = add_button?.previousSibling
+      ?.previousSibling as HTMLInputElement;
+    if (previousSibling.value === "") {
+      console.log("Please enter the value");
+      previousSibling.placeholder = "Please enther the value";
+      previousSibling.style.borderBottom = "2px solid red";
+    } else {
+      console.log("hello Pakistan");
+      console.log();
+      if (previousSibling.previousElementSibling?.innerHTML == "Experience:") {
+        listingforexperince.push(previousSibling.value);
+        console.log(listingforexperince);
+        previousSibling.value = "";
+        previousSibling.placeholder = "Enter your experience";
+        previousSibling.style.borderBottom = "2px solid black";
+        const newHtml = listingforexperince
+          .map(
+            (experience, id) =>
+              `<li id="experience-${id}">${experience}
+              <button data-id="${id}" class="del_button">Delete</button>
+            </li>`
+          )
+          .join("");
+        let divContainer = add_button.nextElementSibling as HTMLDivElement;
+        divContainer.innerHTML = `<ul>${newHtml}</ul>`;
+      }
+      if (
+        previousSibling.previousElementSibling?.innerHTML == "Technical Skills:"
+      ) {
+        listingfortechnicalskills.push(previousSibling.value);
+        console.log(listingfortechnicalskills);
+        previousSibling.value = "";
+        previousSibling.placeholder = "Enter your the value";
+        previousSibling.style.borderBottom = "2px solid black";
+        const newHtml = listingfortechnicalskills
+          .map(
+            (item, id) =>
+              `<li id="experience-${id}">${item}
+              <button data-id="${id}" class="del_button">Delete</button>
+            </li>`
+          )
+          .join("");
+        let divContainer = add_button.nextElementSibling as HTMLDivElement;
+        divContainer.innerHTML = `<ul>${newHtml}</ul>`;
+      }
+      if (
+        previousSibling.previousElementSibling?.innerHTML == "General Skills:"
+      ) {
+        listingforgeneralskills.push(previousSibling.value);
+        console.log(listingforexperince);
+        previousSibling.value = "";
+        previousSibling.placeholder = "Enter your the value";
+        previousSibling.style.borderBottom = "2px solid black";
+        const newHtml = listingforgeneralskills
+          .map(
+            (item, id) =>
+              `<li id="experience-${id}">${item}
+              <button data-id="${id}" class="del_button">Delete</button>
+            </li>`
+          )
+          .join("");
+        let divContainer = add_button.nextElementSibling as HTMLDivElement;
+        divContainer.innerHTML = `<ul>${newHtml}</ul>`;
+      }
+      if (previousSibling.previousElementSibling?.innerHTML == "Hobbies:") {
+        listingforhobbies.push(previousSibling.value);
+        console.log(listingforhobbies);
+        previousSibling.value = "";
+        previousSibling.placeholder = "Enter your the value";
+        previousSibling.style.borderBottom = "2px solid black";
+        const newHtml = listingforhobbies
+          .map(
+            (item, id) =>
+              `<li id="experience-${id}">${item}
+              <button data-id="${id}" class="del_button">Delete</button>
+            </li>`
+          )
+          .join("");
+        let divContainer = add_button.nextElementSibling as HTMLDivElement;
+        divContainer.innerHTML = `<ul>${newHtml}</ul>`;
+      }
+      if (previousSibling.previousElementSibling?.innerHTML == "Languages:") {
+        listingforlanguages.push(previousSibling.value);
+        console.log(listingforlanguages);
+        previousSibling.value = "";
+        previousSibling.placeholder = "Enter your the value";
+        previousSibling.style.borderBottom = "2px solid black";
+        const newHtml = listingforlanguages
+          .map(
+            (item, id) =>
+              `<li id="experience-${id}">${item}
+              <button data-id="${id}" class="del_button">Delete</button>
+            </li>`
+          )
+          .join("");
+        let divContainer = add_button.nextElementSibling as HTMLDivElement;
+        divContainer.innerHTML = `<ul>${newHtml}</ul>`;
+      }
+    }
+  }
+});
+
+// Personal Information
+const user_name = document.getElementById("name_input") as HTMLInputElement;
+const profile_picture_input = document.getElementById(
+  "profile_picture_input"
+) as HTMLInputElement;
+const contactNumber = document.getElementById(
+  "contact_number_input"
+) as HTMLInputElement;
+const email = document.getElementById("email_input") as HTMLInputElement;
+const dob = document.getElementById("dob_input") as HTMLInputElement;
+
+// Family Information
+const fatherName = document.getElementById(
+  "father_name_Input"
+) as HTMLInputElement;
+const cnic = document.getElementById("cnic_input") as HTMLInputElement;
+
+// Professional Information
+const careerObjective = document.getElementById(
+  "career_objective_input"
+) as HTMLInputElement;
+const experience = document.getElementById("experience") as HTMLInputElement;
+const technicalSkills = document.getElementById(
+  "technical_skills_input"
+) as HTMLInputElement;
+const generalSkills = document.getElementById(
+  "general_skills_input"
+) as HTMLInputElement;
+
+// Additional Information
+const languages = document.getElementById(
+  "languages_input"
+) as HTMLInputElement;
+const hobbies = document.getElementById("hobbies_input") as HTMLInputElement;
+const religion = document.getElementById("religion_input") as HTMLInputElement;
+const maritalStatus = document.getElementById(
+  "marital_status_input"
+) as HTMLInputElement;
+
+let name_heading = document.getElementById("name_heading");
+let profile_picture = document.getElementById(
+  "profile_picture"
+) as HTMLImageElement;
+let user_contact = document.getElementById("user_contact");
+let user_email = document.getElementById("user_email");
+let user_Language = document.getElementById("user_Language");
+let user_hobbies = document.getElementById("user_hobbies");
+let career_objective = document.getElementById("career_objective");
+let father_name = document.getElementById("father_name");
+let user_DOB = document.getElementById("user_DOB");
+let user_cnic = document.getElementById("user_cnic");
+let user_maritalStatus = document.getElementById("user_maritalStatus");
+let user_relagion = document.getElementById("user_relagion");
+let user_experience = document.getElementById("user_experience");
+let technical_Skills = document.getElementById("technical_Skills");
+let user_generalSkills = document.getElementById("user_generalSkills");
+
+document.getElementById("submit_button")?.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // First, update the profile picture (if any)
+  const file = profile_picture_input.files?.[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      profile_picture_input.src = e.target?.result as string;
+      if (profile_picture) {
+        profile_picture.src = profile_picture_input.src;
+      }
+    };
+    reader.readAsDataURL(file);
+  }
+
+  if (
+    name_heading &&
+    user_contact &&
+    user_email &&
+    user_Language &&
+    user_hobbies &&
+    career_objective &&
+    father_name &&
+    user_DOB &&
+    user_cnic &&
+    user_maritalStatus &&
+    user_relagion &&
+    user_experience &&
+    technical_Skills &&
+    user_generalSkills
+  ) {
+    name_heading.innerText = user_name.value;
+    user_contact.innerText = contactNumber.value;
+    user_email.innerText = email.value;
+    career_objective.innerText = careerObjective.value;
+    father_name.innerText = fatherName.value;
+    user_DOB.innerText = dob.value;
+    user_cnic.innerText = cnic.value;
+    user_relagion.innerText = religion.value;
+    user_maritalStatus.innerText = maritalStatus.value;
+
+    // Update the list of languages
+    user_Language.innerHTML = listingforlanguages
+      .map((item) => `<li>${item}</li>`)
+      .join("");
+    user_experience.innerHTML = listingforexperince
+      .map((item) => `<li>${item}</li>`)
+      .join("");
+    technical_Skills.innerHTML = listingfortechnicalskills
+      .map((item) => `<li>${item}</li>`)
+      .join("");
+    user_generalSkills.innerHTML = listingforgeneralskills
+      .map((item) => `<li>${item}</li>`)
+      .join("");
+    user_hobbies.innerHTML = listingforhobbies
+      .map((item) => `<li>${item}</li>`)
+      .join("");
   }
 });
